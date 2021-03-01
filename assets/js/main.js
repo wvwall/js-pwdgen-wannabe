9c1baeb15ca631204ea77d30b04267ae696dcf80
wvwall <waltervelardo@gmail.com>
2021-03-01 09:40:07 +0100

    Console log unico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
 /* Chiedi il nome all'utente */
 
 var nome = prompt("INSERISCI IL TUO NOME");
-console.log(nome);
+
 
 /* Chiedi il cognome */
 
 var cognome = prompt("INSERISCI IL TUO COGNOME")
 
-console.log(cognome);
+
 
 /* Chiedi il colore preferito */
 
 var colore = prompt("ADESSO IL TUO COLORE PREFERITO")
 
-console.log(colore);
+
+
+console.log(nome, cognome, colore);
 
 /* Scrivi sulla pagina */
 
